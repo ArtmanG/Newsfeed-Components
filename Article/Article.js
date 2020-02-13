8cd43fd2294a56808c6c1501ba5133e433a2a69a
Gregory Hawman <g.hawman@gmail.com>
2020-02-13 13:55:46 -0800

    finished part 1. creating the creator and adding a new article to the list of articles.

--- Article/Article.js
+++ Article/Article.js
@@ -88,6 +88,18 @@ const data = [
   }
 ];
 
+const newArticle = {
+  title: 'Remember, remember, the 5th of November, the Gunpowder Treason and Plot.',
+  date: 'Nov 5th, 1982',
+  firstParagraph: `Voilà! In view, a humble vaudevillian veteran, cast vicariously as both victim and villain by the vicissitudes of Fate. This visage, no mere veneer of vanity, is a vestige of the vox populi, now vacant, vanished. However, this valorous visitation of a by-gone vexation, stands vivified and has vowed to vanquish these venal and virulent vermin vanguarding vice and vouchsafing the violently vicious and voracious violation of volition. [carves "V" into poster on wall] The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day vindicate the vigilant and the virtuous. Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it's my very good honor to meet you and you may call me V.`,
+
+  secondParagraph: `Eve: All this riot and uproar, V... is this Anarchy? Is this the Land of Do-As-You-Please?
+
+  <br> V: No. This is only the land of take-what-you-want. Anarchy means "without leaders", not "without order". With anarchy comes an age or ordnung, of true order, which is to say voluntary order... this age of ordung will begin when the mad and incoherent cycle of verwirrung that these bulletins reveal has run its course... This is not anarchy, Eve. This is chaos.`,
+
+  thirdParagraph: `I know there\'s no way I can convince you this is not one of their tricks, but I don\'t care, I am me. My name is Valerie, I don\'t think I\'ll live much longer and I wanted to tell someone about my life. This is the only autobiography ill ever write, and god, I\'m writing it on toilet paper. I was born in Nottingham in 1985, I don\'t remember much of those early years, but I do remember the rain. My grandmother owned a farm in Tuttlebrook, and she use to tell me that god was in the rain. I passed my 11th lesson into girl\'s grammar; it was at school that I met my first girlfriend, her name was Sara. It was her wrists. They were beautiful. I thought we would love each other forever. I remember our teacher telling us that is was an adolescent phase people outgrew. Sara did, I didn\'t. In 2002 I fell in love with a girl named Christina. That year I came out to my parents. I couldn\'t have done it without Chris holding my hand. My father wouldn\'t look at me, he told me to go and never come back. My mother said nothing. But I had only told them the truth, was that so selfish? Our integrity sells for so little, but it is all we really have. It is the very last inch of us, but within that inch, we are free. I\'d always known what I wanted to do with my life, and in 2015 I starred in my first film, "The Salt Flats". It was the most important role of my life, not because of my career, but because that was how I met Ruth. The first time we kissed, I knew I never wanted to kiss any other lips but hers again. We moved to a small flat in London together. She grew Scarlet Carsons for me in our window box, and our place always smelled of roses. Those were there best years of my life. But America\'s war grew worse, and worse. And eventually came to London. After that there were no roses anymore. Not for anyone. I remember how the meaning of words began to change. How unfamiliar words like collateral and rendition became frightening. While things like Norse Fire and The Articles of Allegiance became powerful, I remember how different became dangerous. I still don\'t understand it, why they hate us so much. They took Ruth while she was out buying food. I\'ve never cried so hard in my life. It wasn\'t long till they came for me.It seems strange that my life should end in such a terrible place, but for three years, I had roses, and apologized to no one. I shall die here. Every inch of me shall perish. Every inch, but one. An Inch, it is small and it is fragile, but it is the only thing the world worth having. We must never lose it or give it away. We must never let them take it from us. I hope that whoever you are, you escape this place. I hope that the world turns and that things get better. But what I hope most of all is that you understand what I mean when I tell you that even though I do not know you, and even though I may never meet you, laugh with you, cry with you, or kiss you. I love you. With all my heart, I love you. -Valerie`,
+};
+
   function createComponent(title, date, firstParagraph, secondParagraph, thirdParagraph) {
     // define new elements
     const component = document.createElement('div');
@@ -131,6 +143,10 @@ const data = [
 
 const articles = document.querySelector('.articles');
 
+console.log(newArticle);
+data.push(newArticle);
+console.log(data);
+
 data.map(item => {
   articles.appendChild(createComponent(item.title, item.date, item.firstParagraph, item.secondParagraph, item.thirdParagraph))
 });
@@ -156,6 +172,6 @@ data.map(item => {
 
   [x] Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
 
-  [] Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
+  [x] Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
